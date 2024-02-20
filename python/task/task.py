1cf9fe4ffdb3d6425504e0b9047f8ceb938d69e8
Žiga Leber <mail+github@zigaleber.com>
2024-02-20 15:01:16 +0100

    Fix input shadowing

--- python/task/task.py
+++ python/task/task.py
@@ -2,5 +2,5 @@ import sys
 
 if __name__ == '__main__':
     with open(sys.argv[1], "r") as f:
-        input = f.read()
-        print("ANSWER" + input)
+        value = f.read()
+        print("ANSWER" + value)
